4c34f140dc144b00a0510d3c8376971f28ac6b72
Gabe902 <91066565+Gabriels902@users.noreply.github.com>
2023-06-30 17:02:22 -0300

    Delete src directory

--- src/routes/api/pessoaAPIRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const pessoaAPIController = require('../../controllers/api/pessoaAPIController');
-
-router.post('/api/pessoa/cadastrar', pessoaAPIController.cadastrarPessoa);
-
-
-module.exports = router;
--- src/routes/indexRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const indexController = require('../controllers/indexController');
-const autenticacaoController = require('../controllers/autenticacaoController');
-
-router.get('/', autenticacaoController.verificarAutenticacao, indexController.indexView);
-
-module.exports = router;
